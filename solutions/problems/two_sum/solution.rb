744ba9fe01985ae126d88d0ea811cfe30686b9e4
Nate Donato <natedonato@gmail.com>
2019-12-08 22:46:55 +0000

    Sync LeetCode submission - Two Sum (ruby)

--- solutions/problems/two_sum/solution.rb
+++ solutions/problems/two_sum/solution.rb
@@ -1,16 +1,20 @@
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
+
+
+
+
 def two_sum(nums, target)
-    hash = {}
-    nums.each_with_index do |num, idx|
-        hash[num] = idx;
-    end
+    count = Hash.new(false)
     
     nums.each_with_index do |num, idx|
-        pair = target - num
-        if hash[pair] && hash[pair] != idx
-            return [hash[pair], idx]
+        difference = target - num
+        if(count[difference] != false)
+            return[count[difference], idx] 
         end
+        count[num] = idx
     end
+    
+    
 end
