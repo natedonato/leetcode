0949e3d2279f2ff6dc1bddabb059d96e1ec7b62e
Nate Donato <natedonato@gmail.com>
2020-04-10 15:55:52 +0000

    Sync LeetCode submission - Min Stack (ruby)

---
+++ solutions/problems/min_stack/solution.rb
@@ -0,0 +1,61 @@
+class MinStack
+
+=begin
+    initialize your data structure here.
+=end
+    def initialize()
+        @stack = [] 
+        @min = [] 
+        
+    end
+
+
+=begin
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        @stack.push(x)
+        
+        if @min[-1] == nil || @min[-1] > x
+            @min.push(x)
+        else
+            @min.push(@min.last)
+        end
+        
+    end
+
+
+=begin
+    :rtype: Void
+=end
+    def pop()
+        @min.pop()
+        return @stack.pop()
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def top()
+        return @stack.last
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def get_min()
+        return @min.last
+    end
+
+
+end
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack.new()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.get_min()
