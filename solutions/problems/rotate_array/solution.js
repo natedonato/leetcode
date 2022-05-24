4a31e39e1bd1d6f1bc129bf2f76ed00d2234af1d
Nate Donato <natedonato@gmail.com>
2022-05-24 17:39:21 +0000

    Sync LeetCode submission - Rotate Array (javascript)

--- solutions/problems/rotate_array/solution.js
+++ solutions/problems/rotate_array/solution.js
@@ -4,10 +4,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let times = k % nums.length;
-    
-    for(let i = 0; i < times; i++){
-        nums.unshift(nums.pop());
-    }
-    return nums;
+    k = k % nums.length;
+    reverseSection(nums, 0, nums.length-1);
+    reverseSection(nums, 0, k-1);
+    reverseSection(nums, k, nums.length-1);    
 };
+
+// inclusive of endpoint
+var reverseSection = function(arr, start, end){
+    while(start <= end){
+        [arr[start],arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
