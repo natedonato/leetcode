e1b21e988adcef1e97931057880a2f1eed78c1d7
Nate Donato <natedonato@gmail.com>
2022-06-23 20:22:10 +0000

    Sync LeetCode submission - Rotate Array (javascript)

--- solutions/problems/rotate_array/solution.js
+++ solutions/problems/rotate_array/solution.js
@@ -5,16 +5,18 @@
  */
 var rotate = function(nums, k) {
     k = k % nums.length;
+    
     reverseSection(nums, 0, nums.length-1);
     reverseSection(nums, 0, k-1);
-    reverseSection(nums, k, nums.length-1);    
+    reverseSection(nums,k, nums.length-1)
+    
 };
 
-// inclusive of endpoint
-var reverseSection = function(arr, start, end){
-    while(start <= end){
-        [arr[start],arr[end]] = [arr[end], arr[start]];
-        start++;
-        end--;
+// reverses section of array inclusive of endpoints in place
+var reverseSection = function(nums,i,j) {
+    while(i < j){
+        [nums[i],nums[j]] = [nums[j], nums[i]];
+        i++;
+        j--;
     }
-}
+};
