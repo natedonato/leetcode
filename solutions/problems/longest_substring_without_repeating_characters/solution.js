6b6f8fc76b672f873eae3fb04d7db2c84f243940
Nate Donato <natedonato@gmail.com>
2022-06-30 19:20:05 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

--- solutions/problems/longest_substring_without_repeating_characters/solution.js
+++ solutions/problems/longest_substring_without_repeating_characters/solution.js
@@ -1,26 +1,52 @@
-
-var lengthOfLongestSubstring = function (s) {
-    let length = 1;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) { 
     if(s.length === 0){
         return 0;
     }
-    let substring = s[0];
+    
+    let length = 1;    
 
-    for(let i = 1; i < s.length; i++){
-        let char = s[i];
-        substring = checkForInclusion(substring, char).concat(char);
-        if(substring.length > length){
-            length = substring.length;
+    
+    // set two pointers
+    let left = 0;
+    let right = 0;
+    
+    // set character bank
+    let seenChars = {};
+    
+    while(right < s.length){
+        let nextChar = s[right];
+        
+        // console.log("character", nextChar, "index", right);
+        // if the character is already in our substring we hit a duplicate
+        if(seenChars[nextChar] === true){
+            
+            
+            // first take care of the length
+            let subLength = right - left;
+            length = Math.max(length, subLength);
+            
+            // advance the left pointer and remove seen chars
+            while(s[left] !== nextChar){
+                seenChars[s[left]] = false;
+                ++left;
+            } 
+            ++left;
+            
+        }else{
+        // if character isn't in the substring, catalog it and keep growing
+            seenChars[nextChar] = true;
         }
+        
+        ++right;
+            
     }
+    let subLength = right - left;
+    length = Math.max(length, subLength);
+    
     return length;
 };
 
-var checkForInclusion = function (s, c) {
-    const index = s.indexOf(c);
-    if(index === -1){
-        return s;
-    }else{
-        return s.slice(index + 1);
-    }
-};
