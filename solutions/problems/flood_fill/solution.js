02e6ef9768c2f408cb9ebb4b860b1f390674f839
Nate Donato <natedonato@gmail.com>
2022-07-01 01:35:00 +0000

    Sync LeetCode submission - Flood Fill (javascript)

--- solutions/problems/flood_fill/solution.js
+++ solutions/problems/flood_fill/solution.js
@@ -2,42 +2,46 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} newColor
+ * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor) {
-    let oldColor = image[sr][sc];
-    let visited = {};
+var floodFill = function(image, sr, sc, color) {
+    let floodColor = image[sr][sc];
+    if(color === floodColor){return image}
+    let queue = [[sr, sc]];
     
-    const fill = (x,y) => {
-        visited[`${x},${y}`] = true;
-        if(image[x][y] === oldColor){
-            image[x][y] = newColor;
-            
-            if(x-1 >= 0){
-                if(image[x-1][y] === oldColor && !visited[`${x-1},${y}`]){
-                    fill(x-1,y)
-                }
-            }
-            if(x + 1 < image.length){
-                 if(image[x+1][y] === oldColor  && !visited[`${x+1},${y}`]){
-                    fill(x+1,y)
-                }
-            }
-            if(y-1 >= 0){
-                 if(image[x][y-1] === oldColor && !visited[`${x},${y-1}`]){
-                    fill(x,y-1)
-                }
-            }
-            if(y + 1 < image[0].length){
-                if(image[x][y+1] === oldColor && !visited[`${x},${y+1}`]){
-                    fill(x,y+1)
-                }
-            }
-        }   
+    
+    while(queue.length > 0){
+        let current = queue.shift();
+        
+        image[current[0]][current[1]]  = color;
+        
+        if(current[0] + 1 < image.length){
+            if(image[current[0] +1][current[1]] === floodColor){
+                queue.push([current[0] +1,current[1]])
+            }   
+        }
+        
+        if(current[0] - 1 >= 0){
+           if(image[current[0] - 1][current[1]] === floodColor){
+                queue.push([current[0] - 1,current[1]])
+            } 
+        }
+        
+                
+        if(current[1] + 1 < image[0].length){
+            if(image[current[0]][current[1] +1] === floodColor){
+                queue.push([current[0],current[1]+1])
+            }   
+        }
+        
+                   
+        if(current[1] - 1 >= 0){
+            if(image[current[0]][current[1] - 1] === floodColor){
+                queue.push([current[0],current[1] - 1])
+            }   
+        }
     }
     
-    fill(sr, sc)
-
     return image;
 };
