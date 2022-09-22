6e39322f6294d9485bd1f1478c3a16b5aa77489e
Nate Donato <natedonato@gmail.com>
2022-09-22 19:37:03 +0000

    Sync LeetCode submission - Valid Parentheses (javascript)

--- solutions/problems/valid_parentheses/solution.js
+++ solutions/problems/valid_parentheses/solution.js
@@ -3,28 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
     
-    const pairs = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    };
-    
-    let stack = [];
+    const pair_lookup = {
+        ')': '(', 
+        '}': '{', 
+        ']': '['    
+    }
     
     for(let i = 0; i < s.length; i++){
-        let char = s[i];
+        let current_char = s[i];
         
-        if(Object.keys(pairs).includes(char)){
-            stack.push(char);   
+        if(stack.length === 0){
+            stack.push(current_char)
         }else{
-            let lastChar = stack.pop();
-            if(pairs[lastChar] !== char){
-                return false;
+            if(pair_lookup[current_char] === stack[stack.length - 1]){
+                stack.pop();
+            }else{
+                stack.push(current_char)
             }
         }
     }
     
-    return(stack.length === 0);
-    
+    return (stack.length === 0);
 };
