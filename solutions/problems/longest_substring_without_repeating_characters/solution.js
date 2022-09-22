9b682dddf1845cc87b9c31ebc5192d4f60c7efef
Nate Donato <natedonato@gmail.com>
2022-09-22 20:22:11 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

--- solutions/problems/longest_substring_without_repeating_characters/solution.js
+++ solutions/problems/longest_substring_without_repeating_characters/solution.js
@@ -2,51 +2,40 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) { 
-    if(s.length === 0){
-        return 0;
-    }
+var lengthOfLongestSubstring = function(s) {
+    let currently_seen_characters = {};
+    let max = 0;
+    let left_idx = 0;
     
-    let length = 1;    
-
     
-    // set two pointers
-    let left = 0;
-    let right = 0;
-    
-    // set character bank
-    let seenChars = {};
-    
-    while(right < s.length){
-        let nextChar = s[right];
+    // keeping track of our left side pointer, iterate through string character by character
+    for(let i = 0; i < s.length; i++){
+        let next_char = s.charAt(i);
         
-        // console.log("character", nextChar, "index", right);
-        // if the character is already in our substring we hit a duplicate
-        if(seenChars[nextChar] === true){
-            
-            
-            // first take care of the length
-            let subLength = right - left;
-            length = Math.max(length, subLength);
-            
-            // advance the left pointer and remove seen chars
-            while(s[left] !== nextChar){
-                seenChars[s[left]] = false;
-                ++left;
-            } 
-            ++left;
-            
+        // if a character is unseen, mark it as seen and test our max length
+        if(currently_seen_characters[next_char] != true){
+            currently_seen_characters[next_char] = true;
+            max = Math.max(max, i - left_idx + 1);
         }else{
-        // if character isn't in the substring, catalog it and keep growing
-            seenChars[nextChar] = true;
-        }
-        
-        ++right;
             
+            // if a character is seen, increase our left pointer until it hits the previously seen character;
+            
+            let left_char = s.charAt(left_idx)
+            
+            while(left_char !== next_char){
+                // unset characters that are outside our moving leftmost boundary   
+                currently_seen_characters[left_char] = false;
+                left_idx += 1;
+                left_char = s.charAt(left_idx)
+            }
+            
+            // move left pointer past the previously seen character
+            left_idx += 1;
+            currently_seen_characters[next_char] = true;
+        }
     }
-    let subLength = right - left;
-    length = Math.max(length, subLength);
     
-    return length;
+    
+    
+    return max;
 };
-
