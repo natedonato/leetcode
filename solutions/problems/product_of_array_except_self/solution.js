e2390b0150d7e7a5dfd28ebea7a55326ed0364fb
Nate Donato <natedonato@gmail.com>
2022-12-28 21:50:23 +0000

    Sync LeetCode submission - Product of Array Except Self (javascript)

--- solutions/problems/product_of_array_except_self/solution.js
+++ solutions/problems/product_of_array_except_self/solution.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let prod = 1;
-    let arr = []
+    let runningProduct = 1;
     
-    for(let i = nums.length - 1; i >= 0; --i){
-        arr[i] = prod;
-        prod *= nums[i];
+    let output = [1];
+    for(let i = 1; i < nums.length; i++){
+        runningProduct *= nums[i-1];
+        output.push(runningProduct);
+    }
+    runningProduct = 1;
+    for(let i = nums.length - 2; i >= 0; i--){
+        runningProduct *= nums[i+1];
+        output[i] *= runningProduct
     }
     
-    prod = 1;
-    
-    for(let i = 0; i < nums.length; ++i){
-        arr[i] = arr[i] * prod;
-        prod *= nums[i]
-    }
-    
-    return arr
+    return output;
 };
