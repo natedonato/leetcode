c33e2746702bef1ce42ac6d0f088245650ae4765
Nate Donato <natedonato@gmail.com>
2022-12-31 04:49:58 +0000

    Sync LeetCode submission - Reverse Vowels of a String (javascript)

--- solutions/problems/reverse_vowels_of_a_string/solution.js
+++ solutions/problems/reverse_vowels_of_a_string/solution.js
@@ -4,44 +4,26 @@
  */
 var reverseVowels = function(s) {
     s = s.split('');
-    
-    let left = 0
+    let left = 0;
     let right = s.length - 1;
-    let dict = {
-        "a": true,
-        "e": true,
-        "i": true,
-        "o": true,
-        "u": true,
-        "A": true,
-        "E": true,
-        "I": true,
-        "O": true,
-        "U": true
-    };
-    
-    function isVowel(char){
-        return (dict[char] === true) 
-    };
-    
-    
     while(left < right){
-       while(!isVowel(s[left]) && !(left >= s.length -1)){
+        while(left < s.length && !isVowel(s[left])){
             left += 1;
-        };
-        while(!isVowel(s[right]) && !(right <= 0)){
+        }
+        while(right > 0 && !isVowel(s[right])){
             right -= 1;
-        };
+        }
 
-        if(left < right){
-            let temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-        };
-
-        left += 1;
-        right -= 1;
+        if(left <= right){
+            [s[left],s[right]] = [s[right],s[left]]
+            left += 1;
+            right -= 1;
+        }
     }
     
-    return s.join('')
+    return s.join("");
 };
+
+var isVowel = function (char){
+    return'aeiouAEIOU'.includes(char)
+}
