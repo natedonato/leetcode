c3a3124b7714dbcc6517a6baf2a81fcd5eab3b09
Nate Donato <natedonato@gmail.com>
2022-12-31 05:16:02 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (javascript)

--- solutions/problems/median_of_two_sorted_arrays/solution.js
+++ solutions/problems/median_of_two_sorted_arrays/solution.js
@@ -1,50 +1,33 @@
-//[1,2] [3,4]
-// idx = 0
-//midpoint = 2
-//cEle = 
-
- 
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const totalLength = nums1.length + nums2.length;
-    if(totalLength === 1){return nums1.concat(nums2)[0]}
-    const midpoint = totalLength / 2;
-    let index = 0;
-    let currentEle;
-    while(index < midpoint){
+    let length = nums1.length + nums2.length;
+    let mid = Math.floor(length / 2);
+    if(length % 2 !== 0){
+        mid += 1;
+    }
+    let next = null
+    let i = 0;
+    while(i < mid){
+        next = getNext();
+        i += 1;
+    }
+
+    function getNext(){
         if(nums1.length === 0){
-            currentEle = nums2.shift();
-            index++;
-        }else if (nums2.length === 0){
-            currentEle = nums1.shift();
-            index++;
+            return nums2.shift()
+        }else if(nums2.length === 0){
+            return nums1.shift()
         }else{
-            if(nums1[0] < nums2[0]){
-                currentEle = nums1.shift();
-                index++;
-            }else{
-                currentEle = nums2.shift();
-                index++;
-            }
+            return nums1[0] <= nums2[0] ? nums1.shift() : nums2.shift();
         }
     }
-    
-    let nextEle;
-    if (nums1.length === 0) {
-        nextEle = nums2.shift();
-    } else if (nums2.length === 0) {
-        nextEle = nums1.shift();
-
-    } else {
-        if (nums1[0] < nums2[0]) {
-            nextEle = nums1.shift();
-        } else {
-            nextEle = nums2.shift();
-        }
+    if(length % 2 === 0){
+        return (next + getNext()) / 2
     }
 
-    if (totalLength % 2 === 1) {
-        return currentEle;
-    }
-
-    return((currentEle + nextEle) / 2.0);
+    return next;
 };
