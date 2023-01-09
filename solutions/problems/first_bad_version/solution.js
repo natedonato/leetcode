d57bc6c9be6f392c23e10aeb1c633dc0c1a9dece
Nate Donato <natedonato@gmail.com>
2023-01-09 06:04:21 +0000

    Sync LeetCode submission - First Bad Version (javascript)

--- solutions/problems/first_bad_version/solution.js
+++ solutions/problems/first_bad_version/solution.js
@@ -18,21 +18,18 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-    let left = 1;
-    let right = n;
-        
-    while(left < right){
-        let mid = left + Math.floor((right - left)/2);
-        
-        if(isBadVersion(mid)){
-            right = mid;
-        }else{
-            left = mid + 1
+        let left = 1;
+        let right = n;
+
+        while(left < right){
+            let mid = left + Math.floor((right - left) / 2);
+            
+            if(isBadVersion(mid)){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
         }
-        
-        
-        
-    }
-        return left;  
+        return left;
     };
 };
