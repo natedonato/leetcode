8907dafc0f1e4e6e55ce3eec9543559083ff9ab5
Nate Donato <natedonato@gmail.com>
2023-01-09 02:17:16 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- solutions/problems/binary_search/solution.js
+++ solutions/problems/binary_search/solution.js
@@ -4,26 +4,23 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let condition = (value) =>{
-        if(value >= target){
-            return true   
-        }else{
-            return false
-        }
-    }
-    
-    let left = 0
+
+    let left = 0;
     let right = nums.length - 1;
-    
+
     while(left < right){
-        let mid = left + Math.floor((right - left)/2);
-        let val = nums[mid];
-        
-        if(condition(val)){
-            right = mid;   
+        mid = left + Math.floor((right - left) / 2);
+
+        if(nums[mid] >= target){
+            right = mid
         }else{
             left = mid + 1;
         }
     }
-    return(nums[left] === target ? left : -1);
+
+    if(nums[left] === target){
+        return left
+    }else{
+        return -1
+    }
 };
