39ffaee6541ce78c15e118fd90d264b5b9dd9305
Nate Donato <natedonato@gmail.com>
2025-03-11 21:48:38 +0000

    Sync LeetCode submission Runtime - 107 ms (38.47%), Memory - 28.5 MB (67.49%)

--- solutions/0011-container-with-most-water/solution.py
+++ solutions/0011-container-with-most-water/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        area = 0
-
-        while left < right:
-            current_min_height = min(height[left], height[right])
-            current_area = current_min_height * (right - left)
-            area = max(area, current_area)
-
-            
-            while left < len(height) and height[left] <= current_min_height:
-                left += 1
-            while right > 0 and height[right] <= current_min_height:
-                right -= 1
-
-        return area
+        l = 0
+        r = len(height) - 1
+        best = 0
+        
+        while l < r:
+            current = (r - l) * min(height[l], height[r])
+            best = max(current,best)
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+    
+        return best
