06219f283f3b2f8e14b1790a86afd2be2747ca4b
Nate Donato <natedonato@gmail.com>
2025-07-23 18:47:59 +0000

    Sync LeetCode submission Runtime - 43 ms (94.72%), Memory - 54.8 MB (64.14%)

---
+++ solutions/2741-function-composition/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let output = x
+        for(const fn of functions.reverse()){
+            output = fn(output)
+        }
+        return output
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
