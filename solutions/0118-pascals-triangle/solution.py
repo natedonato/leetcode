51d657f499ad8f4b6da2f2e482707374ba6fb226
Nate Donato <natedonato@gmail.com>
2025-08-01 17:36:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (37.23%)

--- solutions/0118-pascals-triangle/solution.py
+++ solutions/0118-pascals-triangle/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        current_row = 1
-        out = []
-
-        while(current_row <= numRows):
-            row = [1] * current_row
-            if(current_row > 2):
-                prev_row = out[-1]
-                for i in range(1, current_row - 1):
-                    row[i] = prev_row[i - 1] + prev_row[i]
-            
-            out.append(row)
-            current_row += 1
+        out = [[1]]
+        while len(out) < numRows:
+            next_row = []
+            next_row.append(1)
+            for i in range(len(out[-1]) - 1):
+                next_row.append(out[-1][i] + out[-1][i+1])
+            next_row.append(1)
+            out.append(next_row)
 
         return out
