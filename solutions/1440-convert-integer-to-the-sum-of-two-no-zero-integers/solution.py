38fad130c623a9609eacb8a61abf49ba72db0996
Nate Donato <natedonato@gmail.com>
2025-09-08 17:49:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (32.06%)

---
+++ solutions/1440-convert-integer-to-the-sum-of-two-no-zero-integers/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        a = n-1
+        b = 1
+
+        while "0" in str(a) or "0" in str(b):
+            a -= 1
+            b += 1
+        
+        return [a,b]
