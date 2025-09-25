37d11b832f649d6072af58c3e4601377d2cad51c
Nate Donato <natedonato@gmail.com>
2025-09-25 22:40:32 +0000

    Sync LeetCode submission Runtime - 3 ms (87.44%), Memory - 19.2 MB (29.96%)

--- solutions/0120-triangle/solution.py
+++ solutions/0120-triangle/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        m = len(triangle)
-        for r in range(m):
-            if(r > 0):
-                triangle[r][0] += triangle[r-1][0]
-                triangle[r][r] += triangle[r-1][r-1]
+        for r in reversed(range(0, len(triangle) - 1)):
+            for c in range(0, len(triangle[r])):
+                triangle[r][c] += min(triangle[r+1][c], triangle[r+1][c+1])
 
-                for i in range(1,r):
-                    triangle[r][i] += min(triangle[r-1][i-1], triangle[r-1][i])
-
-        return min(triangle[m-1])
+        return triangle[0][0]
