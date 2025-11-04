8821c9eb76a975f91d03fcd52185e14df4b24e28
Nate Donato <natedonato@gmail.com>
2025-11-04 20:29:29 +0000

    Sync LeetCode submission Runtime - 3 ms (30.65%), Memory - 19.8 MB (100.00%)

--- solutions/0057-insert-interval/solution.py
+++ solutions/0057-insert-interval/solution.py
@@ -3,19 +3,19 @@ class Solution:
         out = []
         inserted = False
 
-        for interval in intervals:
-            if interval[1] < newInterval[0]:
-                out.append(interval)
-            elif interval[0] > newInterval[1]:
+        for i in intervals:
+            if i[1] < newInterval[0]:
+                out.append(i)
+            elif i[0] > newInterval[1]:
                 if not inserted:
                     out.append(newInterval)
                     inserted = True
-                out.append(interval)
+                out.append(i)
             else:
-                newInterval[0] = min(newInterval[0], interval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+                newInterval[0] = min(newInterval[0],i[0])
+                newInterval[1] = max(newInterval[1], i[1])
 
         if not inserted:
             out.append(newInterval)
-            
+
         return out
