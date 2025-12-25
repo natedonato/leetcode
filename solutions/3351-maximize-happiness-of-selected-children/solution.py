a4cbc10b475902987ba5800e1b6b629876f02a75
Nate Donato <natedonato@gmail.com>
2025-12-25 00:29:22 +0000

    Sync LeetCode submission Runtime - 838 ms (14.86%), Memory - 44.2 MB (99.11%)

---
+++ solutions/3351-maximize-happiness-of-selected-children/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse = True)
+        total = 0
+        for i in range(k):
+            total += max(0, happiness[i] - i)
+        return total
