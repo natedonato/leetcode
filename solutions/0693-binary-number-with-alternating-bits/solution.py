ffaeead1a558c3c9068f998c3babd99d4e8e0ec8
Nate Donato <natedonato@gmail.com>
2026-02-18 00:29:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (29.32%)

---
+++ solutions/0693-binary-number-with-alternating-bits/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        n = bin(n)
+        for i in range(3, len(n)):
+            if n[i] == n[i-1]:
+                return False
+
+        return True
