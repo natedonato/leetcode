66785f185b2e4e99f328f9ce9b607f80d460f5fc
Nate Donato <natedonato@gmail.com>
2026-03-29 22:25:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (68.97%)

---
+++ solutions/2999-check-if-strings-can-be-made-equal-with-operations-i/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+
+        e1 = ""
+        e2 = ""
+        o1 = ""
+        o2 = ""
+
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                e1 += s1[i]
+                e2 += s2[i]
+            else:
+                o1 += s1[i]
+                o2 += s2[i]
+
+        e1 = "".join(sorted(e1))
+        e2 = "".join(sorted(e2))
+        o1 = "".join(sorted(o1))
+        o2 = "".join(sorted(o2))
+
+        return (e1 == e2) and (o1 == o2)
