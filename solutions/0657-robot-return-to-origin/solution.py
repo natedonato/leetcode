6d464ccb07ed4c92be80c3541bd2fc2f5388c9d0
Nate Donato <natedonato@gmail.com>
2026-04-05 01:47:45 +0000

    Sync LeetCode submission Runtime - 15 ms (56.08%), Memory - 19.4 MB (19.42%)

---
+++ solutions/0657-robot-return-to-origin/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        v = 0
+        h = 0
+        for c in moves:
+            if c == "U":
+                v += 1
+            elif c == "D":
+                v -= 1
+            elif c == "R":
+                h += 1
+            elif c == "L":
+                h -= 1
+
+        return (v == 0) and (h == 0)
+            
