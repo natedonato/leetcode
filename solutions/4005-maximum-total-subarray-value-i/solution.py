1d1a12813b9f47ff909baa120a66620a03175df1
Nate Donato <natedonato@gmail.com>
2026-06-09 01:17:56 +0000

    Sync LeetCode submission Runtime - 15 ms (42.26%), Memory - 26.5 MB (66.81%)

--- solutions/4005-maximum-total-subarray-value-i/solution.py
+++ solutions/4005-maximum-total-subarray-value-i/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        min_val = min(nums)
-        max_val = max(nums)
-
-        return (max_val - min_val) * k
+        mv = max(nums)
+        minv = min(nums)
+        return (mv - minv) * k
