33722c56fca37360fb00dacae536c36e5ad2890f
Nate Donato <natedonato@gmail.com>
2026-06-16 00:27:54 +0000

    Sync LeetCode submission Runtime - 31 ms (60.97%), Memory - 25.9 MB (48.67%)

---
+++ solutions/3931-process-string-with-special-operations-i/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+        for c in s:
+            if c == "*":
+                if res:
+                    res.pop()
+            elif c == "#":
+                res += res
+            elif c == "%":
+                res.reverse()
+            else:
+                res.append(c)
+        
+        return "".join(res)
